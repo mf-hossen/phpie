2f8f8b2a6d3ea36186f7cbbcad8e8436b165c207
Farhad <me.mfhossen@gmail.com>
2016-12-20 18:48:53 +0600

    remove junk code

--- src/Text/Text.php
+++ src/Text/Text.php
@@ -159,7 +159,7 @@ class Text
      * @param $value
      * @return  return  camelcase to snack.
      */
-    public static function snake($value, $delimiter = '.')
+    public static function snake($value)
     {
         preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $value, $matches);
         $ret = $matches[0];
