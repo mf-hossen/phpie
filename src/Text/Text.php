2a6f6e80a776a32ad5ba5a7de0bedcf564343236
Farhad <me.mfhossen@gmail.com>
2016-12-21 13:03:27 +0600

    implement cake  php  Inflector  and removed unnecessary  code

--- src/Text/Text.php
+++ src/Text/Text.php
@@ -2,86 +2,10 @@
 
 namespace Previewtechs\Phpie\Text;
 
-class Text
+use Cake\Utility\Inflector;
+
+class Text extends Inflector
 {
-
-    static $plural = array(
-        '/(quiz)$/i' => "$1zes",
-        '/^(ox)$/i' => "$1en",
-        '/([m|l])ouse$/i' => "$1ice",
-        '/(matr|vert|ind)ix|ex$/i' => "$1ices",
-        '/(x|ch|ss|sh)$/i' => "$1es",
-        '/([^aeiouy]|qu)y$/i' => "$1ies",
-        '/(hive)$/i' => "$1s",
-        '/(?:([^f])fe|([lr])f)$/i' => "$1$2ves",
-        '/(shea|lea|loa|thie)f$/i' => "$1ves",
-        '/sis$/i' => "ses",
-        '/([ti])um$/i' => "$1a",
-        '/(tomat|potat|ech|her|vet)o$/i' => "$1oes",
-        '/(bu)s$/i' => "$1ses",
-        '/(alias)$/i' => "$1es",
-        '/(octop)us$/i' => "$1i",
-        '/(ax|test)is$/i' => "$1es",
-        '/(us)$/i' => "$1es",
-        '/s$/i' => "s",
-        '/$/' => "s"
-    );
-
-    static $singular = array(
-        '/(quiz)zes$/i' => "$1",
-        '/(matr)ices$/i' => "$1ix",
-        '/(vert|ind)ices$/i' => "$1ex",
-        '/^(ox)en$/i' => "$1",
-        '/(alias)es$/i' => "$1",
-        '/(octop|vir)i$/i' => "$1us",
-        '/(cris|ax|test)es$/i' => "$1is",
-        '/(shoe)s$/i' => "$1",
-        '/(o)es$/i' => "$1",
-        '/(bus)es$/i' => "$1",
-        '/([m|l])ice$/i' => "$1ouse",
-        '/(x|ch|ss|sh)es$/i' => "$1",
-        '/(m)ovies$/i' => "$1ovie",
-        '/(s)eries$/i' => "$1eries",
-        '/([^aeiouy]|qu)ies$/i' => "$1y",
-        '/([lr])ves$/i' => "$1f",
-        '/(tive)s$/i' => "$1",
-        '/(hive)s$/i' => "$1",
-        '/(li|wi|kni)ves$/i' => "$1fe",
-        '/(shea|loa|lea|thie)ves$/i' => "$1f",
-        '/(^analy)ses$/i' => "$1sis",
-        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => "$1$2sis",
-        '/([ti])a$/i' => "$1um",
-        '/(n)ews$/i' => "$1ews",
-        '/(h|bl)ouses$/i' => "$1ouse",
-        '/(corpse)s$/i' => "$1",
-        '/(us)es$/i' => "$1",
-        '/s$/i' => ""
-    );
-
-    static $irregular = array(
-        'move' => 'moves',
-        'foot' => 'feet',
-        'goose' => 'geese',
-        'sex' => 'sexes',
-        'child' => 'children',
-        'man' => 'men',
-        'tooth' => 'teeth',
-        'person' => 'people',
-        'valve' => 'valves'
-    );
-
-    static $uncountable = array(
-        'sheep',
-        'fish',
-        'deer',
-        'series',
-        'species',
-        'money',
-        'rice',
-        'information',
-        'equipment'
-    );
-
 
     /**
      * @param $string
@@ -113,14 +37,13 @@ class Text
     }
 
     /**
-     * @param $value
+     * @param string $string
      * @param string $delimiter
      * @return string
      */
-    public static function slug($string, $delimiter = '_')
+    public static function slug($string, $delimiter = '-')
     {
-        $explodeValue = explode(' ', $string);
-        return join($delimiter, $explodeValue);
+        return Inflector::slug($string, $delimiter);
     }
 
     /**
@@ -147,16 +70,6 @@ class Text
 
     /**
      * @param $value
-     * @return string
-     */
-    public static function camel($value)
-    {
-        $manipulateStr = str_replace(['-', '_', '.'], ' ', $value);
-        return lcfirst(join('', explode(' ', ucwords($manipulateStr))));
-    }
-
-    /**
-     * @param $value
      * @return  return  camelcase to snack.
      */
     public static function snake($value)
@@ -167,61 +80,6 @@ class Text
             $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
         }
         return implode('_', $ret);
-    }
-
-    /**
-     * @param $value
-     * @return  return humanize string...
-     */
-    public static function humanize($value)
-    {
-        return $manipulateStr = str_replace(['-', '_', '.', '/'], ' ', $value);
-    }
-
-
-    /**
-     * @param $string
-     * @return mixed
-     */
-    public static function plural($string)
-    {
-        if (in_array(strtolower($string), self::$uncountable))
-            return $string;
-
-        foreach (self::$irregular as $pattern => $result) {
-            $pattern = '/' . $pattern . '$/i';
-
-            if (preg_match($pattern, $string))
-                return preg_replace($pattern, $result, $string);
-        }
-        foreach (self::$plural as $pattern => $result) {
-            if (preg_match($pattern, $string))
-                return preg_replace($pattern, $result, $string);
-        }
-
-        return $string;
-    }
-
-
-    /**
-     * @param $string
-     * @return mixed
-     */
-    public static function singular($string)
-    {
-        if (in_array(strtolower($string), self::$uncountable))
-            return $string;
-        foreach (self::$irregular as $result => $pattern) {
-            $pattern = '/' . $pattern . '$/i';
-
-            if (preg_match($pattern, $string))
-                return preg_replace($pattern, $result, $string);
-        }
-        foreach (self::$singular as $pattern => $result) {
-            if (preg_match($pattern, $string))
-                return preg_replace($pattern, $result, $string);
-        }
-        return $string;
     }
 
     /**
@@ -250,6 +108,9 @@ class Text
         );
     }
 
+    /**
+     * @return string
+     */
     public static function uuid8()
     {
         return sprintf(
--- tests/i18n/TestTests.php
+++ tests/i18n/TestTests.php
@@ -4,5 +4,14 @@ use Previewtechs\Phpie\Text\Text;
 
 class TestTests extends PHPUnit_Framework_TestCase
 {
+    public function testSample()
+    {/*
+       $result =  Text::slug(     'ManHh dsd dssc    ', '+');
+        var_dump($result);*/
+
+        $result = \Cake\Utility\Inflector::tableize('tbl');
+        var_dump($result);
+
+    }
 
 }
